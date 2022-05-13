1a65e88b153f1a10af45d6e85aad06eaf51bd2f7
Ashley Davis <ashley@codecapers.com.au>
2022-05-13 19:52:40 +1000

    Insta-mongo is working!

--- README.md
+++ README.md
@@ -1,115 +1,125 @@
-# typescript-template
+# insta-mongo
 
-A template for a TypeScript app. To make it easier to start a new app without having to go through all the configuration.
+Instantly start a MongoDB dev server and load database fixtures.
 
-[Click here to support my work](https://www.codecapers.com.au/about#support-my-work)
+You need [Node.js](https://nodejs.org/) installed to use `insta-mongo`.
 
-## Features
+## Install it
 
-- Example module.
-- Example command line app (installable via npm install -g)
-- Testing using Jest.
-- Debugging setup for VS Code.
+Install globally:
+
+```bash
+npm install -g insta-mongo
+```
+
+Install locally to use as a development database for your Node.js project:
+
+```bash
+npm install --save-dev insta-mongo
+```
+
+## Run it
+
+In your terminal, run it globally:
+
+```bash
+insta-mongo
+```
+
+Or run it locally:
+
+```bash
+npx insta-mongo
+```
+
+You now have a MongoDB database server with a REST API to load database fixtures*.
+
+\* A database fixture is a particular named set of test data for your application.
+
+## Integrate it in your Node.js project
+
+To create a dev database that runs while your Node.js application is running use [concurrently](https://www.npmjs.com/package/concurrently).
+
+Your `start` and `start:dev` script in your `package.json` will look something like this:
+
+```json
+{
+    "scripts": {
+        "start": "node index.js",
+        "start-db": "insta-mongo"
+        "start:dev": "concurrently \"npm run start-db\" \"npm start\" --kill-others"
+    }
+}
+```
+
+Using the flag `--kill-others` will stop the database server when your Node.js application terminates.
+
 
 ## Usage
 
-When you want to start a new TypeScript app:
-
-- Copy this repo
-- Search and replace 'typescript-template' to 'your-module-name' across the entire repo
-- Install your own custom dependencies
-- Add your custom code.
-- Add to your own Github or Bitbucket repo (you can npm install from a Git repo! Even a private one!)
-- If necessary, publish to npm using `npm publish`.
-
-You now have a reusable code module and/or command line app that you can 'npm install' and share with your team mates.
-
-## Get the code
-
-Clone or download and unpack the repo.
-
-Install local dependencies
-
-    cd your-module-name
-    npm install
-
-## Installation
-
-Once you publish you can install via npm and use it from TypeScript or JavaScript or from the command line.
-
-### From code
-
-Import and use it (in a TypeScript file):
-
-```typescript
-import { ExampleClass } from 'your-module-name';
-
-var example = new ExampleClass();
-console.log(example.returnsTrue());
+```bash
+[npx] insta-mongo [options]
 ```
 
-Import and use it (from JavaScript):
+```
+Options:
 
-```javascript
-var yourModule = require('your-module-name');
-var ExampleClass = yourModule.ExampleClass;
-
-var example = new ExampleClass();
-console.log(example.returnsTrue());
+ --db-port=<port-no>            Sets the port for the database server.
+                                Defaults to 5001.
+ --rest-port=<port-no>          Sets the port for the REST API      
+                                Defaults to 5000.
+ --db=<database-name>           Sets the database into which to load 
+                                the initial fixture.
+ --load=<fixture-name>          Loads an initial named database fixture.
+ --fixtures=<path-to-fixtures>  Sets the path that contains database fixtures.
+                                Defaults to ./fixtures.
 ```
 
-### From command line
+## Database fixtures:
 
-You can also run your published module as a command line app.
+Place your database fixtures under ./fixtures like this:
 
-For example, install it globally:
+```
+./fixtures/
+    fixture-1/
+        collection1.json
+        collection2.json
+    fixture-2/
+        another-collection.js
+```
 
-    npm install -g your-module-name
+Each JavaScript or JSON file is loaded into it's own collection that is the same as the name of the file.
 
-Then run it:
+[See example JavaScript collection here](todo).
 
-    your-module-name-cli [args]
+[See example JSON collection here](todo).
 
-## Building the code
+## REST API end points
 
-Open folder in Visual Studio Code and hit Ctrl+Shift+B
+Hit the following endpoints to load and unload your database fixtures.
 
-Or
+Loads a fixture:
+  
+```
+HTTP GET http://localhost:5000/load-fixture?db=<db-name>&fix=<your-fixture-name>
+```
 
-    cd typescript-template
-    npm run build
+Unloads a fixture:
 
-## Debugging
+```
+HTTP GET http://localhost:5000/unload-fixture?db=<db-name>&fix=<your-fixture-name>
+```
 
-- Open in Visual Studio Code.
-- Select 'Main' debug configuration.
-- Select the 'Test All' or 'Test Current' debug configuration to debug all tests or the current test file.
-- Set your breakpoints.
-- Hit F5 to run.
+Drops a named database collection:
 
-## Build and run
+```
+HTTP GET http://localhost:5000/drop-collection?db=<db-name>&col=<collection-name>
+```
 
-Compile the application:
+Gets the contents of a collection.
 
-    npm run build
+```
+HTTP GET http://localhost:5000/get-collection?db=<db-name>&col=<collection-name>
+```
 
-The run the compiled JavaScript:
-
-    npm start
-
-## Running without building
-
-Run the command line app directly:
-
-    npm start:dev
-
-Run tests directly:
-
-    npm test
-
-Or:
-
-    npm run test:watch
-
-
-**Checkout** package.json for more scripts!
+[See a VS Code REST Client script that exercises these REST APIs](todo).
--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,302 @@
-import { ExampleClass } from "./index";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import * as express from "express";
+import { Express, Request, Response } from "express";
+import { Server } from 'http';
+import { AddressInfo } from 'net';
+import * as Fixtures from 'node-mongodb-fixtures';
+import * as path from "path";
+import * as mongodb from "mongodb";
+import { MongoClient } from 'mongodb';
+import * as minimist from "minimist";
 
-(new ExampleClass()).test();
+async function main() {
 
-//TODO: Code for your command line app goes here.
+    const argv = minimist(process.argv.slice(2));
+
+    if (argv['?'] || argv.h || argv.help) {
+        displayHelp();
+        return;
+    }
+
+    const mongoPort = argv["db-port"] && parseInt(argv["db-port"]) || 5001;
+
+    const mongod = await MongoMemoryServer.create({
+        instance: {
+            port: mongoPort,
+        },
+    });
+
+    const serverUri = mongod.getUri();
+
+    console.log(`MongoDB server running at ${serverUri}`);
+
+    const dbClient = await mongodb.MongoClient.connect(serverUri);
+
+    const app = express();
+
+    const fixturesDirectory = argv.fixtures && path.resolve(argv.fixtures) || path.join(process.cwd(), "fixtures");
+    console.log(`\r\nLoading database fixtures from ${fixturesDirectory}`);
+
+    initRoutes(app, dbClient, serverUri, fixturesDirectory);
+
+    const restApiPort = argv["rest-port"] && parseInt(argv["rest-port"]) || 5000;
+
+    await startServer(app, restApiPort);
+
+    console.log(`\r\nREST API running at http://localhost:${restApiPort}`);
+    displayRestApiHelp(restApiPort);
+
+    if (argv.load) {
+        if (!argv.db) {
+            throw new Error(`To load an initial database fixture please use --db=<database-name> to specify which database to load the fixture into.`);
+        }
+
+        await loadFixture(serverUri, argv.db, argv.load, fixturesDirectory);
+    }
+}
+
+main()
+    .catch(err => {
+        console.error(`insta-mongo failed to start:`);
+        console.error(err);
+    });
+
+//
+// Display help for the user.
+//
+function displayHelp() {
+    console.log(`
+Usage:
+  
+ [npx] insta-mongo [options]
+
+Examples:
+
+ insta-mongo\t\t\tStart an instant MongoDB database and REST API.
+ insta-mongo --rest-port=3200\tSet the port of the REST API.
+ insta-mongo --db-port=3210\tSet the port of the database server.
+ insta-mongo --load=fixture-1\tStart an instant database and load a particular fixture.
+
+Options:
+
+ --db-port=<port-no>\t\tSets the port for the database server (defaults to 5001).
+ --rest-port=<port-no>\t\tSets the port for the REST API (defaults to 5000).
+ --db=<database-name>\t\tSets the database into which to load the initial fixture.
+ --load=<fixture-name>\t\tLoads an initial named database fixture.
+ --fixtures=<path-to-fixtures>\tSets the path that contains database fixtures (defaults to ./fixtures).
+
+Database fixtures:
+
+ Place your database fixtures under ./fixtures like this:
+
+ ./fixtures
+ \t/fixture-1
+ \t\tcollection1.json
+ \t\tcollection2.json
+ \t/fixture-2
+ \t\tanother-collection.json
+
+ Each JSON file is loaded into it's own collection.
+
+Rest API:`);
+
+    displayRestApiHelp(5000);
+}
+
+//
+// Display help for the REST API.
+//
+function displayRestApiHelp(restApiPort: number): void {
+    console.log(`
+ Use the following endpoints to load and unload your database fixtures:
+  
+ HTTP GET http://localhost:${restApiPort}/load-fixture?db=<db-name>&fix=<your-fixture-name>
+ HTTP GET http://localhost:${restApiPort}/unload-fixture?db=<db-name>&fix=<your-fixture-name>
+ HTTP GET http://localhost:${restApiPort}/drop-collection?db=<db-name>&col=<collection-name>
+ HTTP GET http://localhost:${restApiPort}/get-collection?db=<db-name>&col=<collection-name>
+    `);
+}
+
+//
+// Starts the HTTP server.
+//
+function startServer(app: Express, port: number): Promise<{
+        server: Server,
+        host: string,
+        port: number,
+    }> {
+    return new Promise((resolve, reject) => {
+        var server = app.listen(port, () => {
+            const addrInfo = server.address() as AddressInfo;
+            const host = addrInfo.address;
+            const port = addrInfo.port;
+            resolve({
+                server,
+                host,
+                port
+            });
+        });
+    });
+}
+
+//
+// Loads a fixture to the database.
+//
+async function loadFixture(serverUri: string, databaseName: string, fixtureName: string, fixturesDirectory: string) {
+    const fixtures = new Fixtures({
+        dir: path.join(fixturesDirectory, fixtureName),
+        mute: false,
+    });
+
+    await fixtures.connect(serverUri + databaseName);
+    await fixtures.unload();
+    await fixtures.load();
+    await fixtures.disconnect();
+}
+
+//
+// Unloads a fixture from the database.
+//
+async function unloadFixture(serverUri: string, databaseName: string, fixtureName: string, fixturesDirectory: string) {
+    const fixtures = new Fixtures({
+        dir: path.join(fixturesDirectory, fixtureName),
+        mute: false,
+    });
+
+    await fixtures.connect(serverUri + databaseName);
+    await fixtures.unload();
+    await fixtures.disconnect();
+}
+
+//
+// Determine if a particular named collection already exists.
+//
+// Source: https://stackoverflow.com/questions/21023982/how-to-check-if-a-collection-exists-in-mongodb-native-nodejs-driver
+//
+async function collectionExists(client: mongodb.MongoClient, databaseName: string, collectionName: string) {
+    const db = client.db(databaseName);
+    const collectionNames = await db.listCollections().toArray();
+    return collectionNames.some(collection => collection.name === collectionName);
+}
+
+//
+// Drop a collection if it exists.
+//
+async function dropCollection(client: mongodb.MongoClient, databaseName: string, collectionName: string) {
+    const collectionAlreadyExists = await collectionExists(client, databaseName, collectionName);
+    if (collectionAlreadyExists) {
+        const db = client.db(databaseName);
+        await db.dropCollection(collectionName);
+        console.log("Dropped collection: " + collectionName);
+    }
+    else {
+        console.log("Collection doesn't exist: " + collectionName);
+    }
+}
+
+//
+// Initializes REST API routes.
+//
+function initRoutes(app: Express, dbClient: MongoClient, serverUri: string, fixturesDirectory: string) {
+
+    //
+    // Checks that the API is alive.
+    //
+    app.get("/is-alive", (req, res) => {
+        res.json({
+            ok: true,
+        });
+    });
+
+    function verifyQueryParam(req: Request, res: Response, paramName: string, msg: string): string {
+        const param = req.query[paramName];
+        if (!param) {
+            res.status(400).send(msg);
+        }
+        return param as string;
+    }
+
+    app.get("/load-fixture", async (req, res) => {
+        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
+        const fixtureName = verifyQueryParam(req, res, "fix", "Query parameter 'fix' specifies name of fixture to load into database."); 
+        if (!databaseName || !fixtureName) {
+            return;
+        }
+
+        try {
+            await loadFixture(serverUri, databaseName, fixtureName, fixturesDirectory)
+
+            console.log("Loaded database fixture: " + fixtureName + " to database " + databaseName);
+            res.sendStatus(200);
+        }
+        catch (err: any) {
+            const msg = "Failed to load database fixture " + fixtureName + " to database " + databaseName;
+            console.error(msg);
+            console.error(err && err.stack || err);
+            res.status(400).send(msg);
+        }
+    });
+
+    app.get("/unload-fixture", async (req, res) => {
+        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
+        const fixtureName = verifyQueryParam(req, res, "fix", "Query parameter 'fix' specifies name of fixture to load into database."); 
+        if (!databaseName || !fixtureName) {
+            return;
+        }
+
+        try {
+            await unloadFixture(serverUri, databaseName, fixtureName, fixturesDirectory);
+
+            console.log("Unloaded database fixture: " + fixtureName + " from database " + databaseName);
+            res.sendStatus(200);
+        }
+        catch (err: any) {
+            const msg = "Failed to unload database fixture " + fixtureName + " from database " + databaseName;
+            console.error(msg);
+            console.error(err && err.stack || err);
+            res.status(400).send(msg);
+        }
+    });
+    
+    app.get("/drop-collection", async (req, res) => {
+        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
+        const collectionName = verifyQueryParam(req, res, "col", "Query parameter 'col' specifies name of collection to drop."); 
+        if (!databaseName || !collectionName) {
+            return;
+        }
+
+        try {
+            await dropCollection(dbClient, databaseName, collectionName);
+
+            console.log(`Dropped collection ${collectionName} from database ${databaseName}`);
+            res.sendStatus(200);
+        }
+        catch (err: any) {
+            const msg = "Failed to drop collection " + collectionName + " from database " + databaseName;
+            console.error(msg);
+            console.error(err && err.stack || err);
+            res.status(400).send(msg);
+        }
+    });
+
+    app.get("/get-collection", async (req, res) => {
+        const databaseName = verifyQueryParam(req, res, "db", "Query parameter 'db' specifies database name."); 
+        const collectionName = verifyQueryParam(req, res, "col", "Query parameter 'col' specifies name of collection to drop."); 
+        if (!databaseName || !collectionName) {
+            return;
+        }
+
+        try {
+            const db = dbClient.db(databaseName);
+            const documents = await db.collection(collectionName).find().toArray();
+            res.json(documents);
+        }
+        catch (err: any) {
+            const msg = "Failed to get collection " + collectionName + " from database " + databaseName;
+            console.error(msg);
+            console.error(err && err.stack || err);
+            res.status(400).send(msg);
+        }
+    });
+
+}
